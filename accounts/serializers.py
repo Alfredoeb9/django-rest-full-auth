835c717667513d28c4213e5e4c54556f2a869ae5
Alfredoeb9 <alfredoeb96@gmail.com>
2024-11-07 15:37:19 -0800

    [Register] created register serializer and register view
    created register serializer to that takes in necessary fields and created validated function to check against both passwords and created function to create user

---
+++ accounts/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import User
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password', 'password2')
+        extra_kwargs = {'password': {'required': True, 'write_only': True, 'min_length': 3, 'max_length': 68}, 'password2': {'required': True, 'write_only': True, 'min_length': 3, 'max_length': 68}}
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': 'Password fields did not match'})
+        
+        return attrs
+    
+    def create(self, validated_data):
+        del validated_data['password2']
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .serializers import UserRegisterSerializer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 # Create your views here.
+
+class RegisterView(CreateAPIView):
+    serializer_class = UserRegisterSerializer
+
+    def post(self, request):
+        user_data = request.data
+        serializer = self.serializer_class(data=user_data)
+
+        if serializer.is_valid():
+            serializer.save()
+            user = serializer.data
+            # send email function user['email]
+            return Response({
+                'data': user,
+                'message': 'User has been created successfully'
+            }, status=HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
