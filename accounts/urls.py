94fd5b7171d3c2e5271de22b9d35be0f3e657a5c
Alfredoeb9 <alfredoeb96@gmail.com>
2024-11-07 18:46:40 -0800

    [verify] created verify-email route and logic
    created verify-email route and logic to check if user has otp in email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, VerifyUserEmail
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('verify-email/', VerifyUserEmail.as_view(), name='verify'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 
 from accounts.utils import send_otp_email
 from .serializers import UserRegisterSerializer
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from .models import OneTimePassword
 
-# Create your views here.
+
+# THIS IS WHAT GETS HIT WHEN A USER REGISTERS
 
 class RegisterView(CreateAPIView):
     serializer_class = UserRegisterSerializer
@@ -28,3 +30,24 @@ class RegisterView(CreateAPIView):
             }, status=HTTP_201_CREATED)
         
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+class VerifyUserEmail(GenericAPIView):
+    def post(self, request):
+        otp_code = request.data.get('otp')
+
+        if not otp_code:
+            return Response({'error': 'OTP is required'}, status=HTTP_400_BAD_REQUEST)
+        
+        try:
+            user_otp = OneTimePassword.objects.get(otp=otp_code)
+            user = user_otp.user
+
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+                return Response({'message': 'User has been verified successfully'}, status=HTTP_201_CREATED)
+            
+            return Response({'message': 'User is already verified'}, status=HTTP_204_NO_CONTENT)
+        
+        except OneTimePassword.DoesNotExist:
+            return Response({'error': 'Invalid OTP'}, status=HTTP_400_BAD_REQUEST)
