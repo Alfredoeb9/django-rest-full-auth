011e2e0a215394bba520de642b1ed475825e6b57
Alfredoeb9 <alfredoeb96@gmail.com>
2024-11-08 19:30:09 -0800

    [bug] fixed login bug with google

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, VerifyUserEmail, LoginUserView, PasswordResetRequestView, PasswordResetConfirm, SetNewPassword, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('verify-email/', VerifyUserEmail.as_view(), name='verify'),
     path('login/', LoginUserView.as_view(), name='login'),
+    # path('profile/', TestAuthtication.as_view(), name='profile'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token'),
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name='password-reset-confirm'),
     path('set-new-password/', SetNewPassword.as_view(), name='set-new-password'),
